063ea843aff5e7e36786ed8ffcf2427930103954
Heber Ortiz Pereira <heber.pereira@lambda3.com.br>
2015-08-17 20:26:40 -0300

    Hello World salvando texto em arquivo

---
+++ Aulas/Mod01/01.HelloWorld/01.HelloWorld/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("01.HelloWorld")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("01.HelloWorld")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7ea737c6-b234-4e76-b550-c8201f8e59ba")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Aulas/Mod01/01.HelloWorld/01.HelloWorld/Startup.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO
+
+Module Startup
+
+    Sub Main()
+        Console.WriteLine("Digite seu texto <Linha Vazia> para salvar)")
+
+        Dim Writer As StreamWriter = File.CreateText("texto.txt")
+
+        Dim Linha As String = Console.ReadLine()
+
+        While Linha <> ""
+            Writer.WriteLine(Linha)
+            Linha = Console.ReadLine()
+        End While
+
+        Writer.Close()
+        Console.WriteLine("Arquivo salvo...")
+        Console.ReadKey()
+
+    End Sub
+
+End Module

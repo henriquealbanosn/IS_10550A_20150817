8e42cae45cd8e11a91dec1096c2a64f591622f5b
Heber Ortiz Pereira <heber.pereira@lambda3.com.br>
2015-08-21 21:15:43 -0300

    Polimorfismo e verificacao de tipo

--- Aulas/Mod08/EstudoClasses/ContaBancaria/ContaCorrente.vb
+++ Aulas/Mod08/EstudoClasses/ContaBancaria/ContaCorrente.vb
@@ -1,3 +1,18 @@
 ﻿Public Class ContaCorrente
     Inherits Conta
+
+    Private _cestaServicos As String
+    Public Property CestaServicos() As String
+        Get
+            Return _cestaServicos
+        End Get
+        Set(ByVal value As String)
+            _cestaServicos = value
+        End Set
+    End Property
+
+    Public Sub New()
+        Me.CestaServicos = "Cesta Basica"
+    End Sub
+
 End Class
--- Aulas/Mod08/EstudoClasses/EstudoClasses/Startup.vb
+++ Aulas/Mod08/EstudoClasses/EstudoClasses/Startup.vb
@@ -7,11 +7,27 @@ Module Startup
         Dim corrente As New ContaCorrente()
         Dim poupanca As New ContaPoupanca()
 
-        poupanca.Deposito(100)
-        corrente.Deposito(100)
+        Depositar(poupanca, 100)
+        Depositar(corrente, 100)
 
-        Console.WriteLine(corrente.Saldo)
-        Console.WriteLine(poupanca.Saldo)
+        ExibirConta(corrente)
+        ExibirConta(poupanca)
+    End Sub
+
+    Sub Depositar(conta As Conta, valor As Decimal)
+        Console.WriteLine("Deposito na conta de valor {0:C}", valor)
+        conta.Deposito(valor)
+    End Sub
+
+    Sub ExibirConta(conta As Conta)
+        Console.WriteLine("Tipo  : {0}", conta.ToString())
+        Console.WriteLine("Numero: {0}", conta.Numero)
+        Console.WriteLine("Saldo : {0}", conta.Saldo)
+        If TypeOf conta Is ContaCorrente Then
+            Dim contaCorrente As ContaCorrente
+            contaCorrente = CType(conta, ContaCorrente)
+            Console.WriteLine("Cesta : {0}", contaCorrente.CestaServicos)
+        End If
     End Sub
 
 End Module
